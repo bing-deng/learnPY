bdf7943fc06aaa9eae5af0855d1496866004c1b5
dengbb <804433988@qq.com>
2020-03-24 15:38:31 +0900

    exam

---
+++ Exam.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+class Exam():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def post(self, answer):
+        lines = open(self.filename,'r')
+        self.tel("3s后开始测试")
+        time.sleep(3)
+        for index,i in enumerate(lines):
+            if answer or index % 2 == 0:
+                self.tel(i)
+            time.sleep(5)
+        self.tel("考试结束")
+
+
+    def tel(self, msg):
+
+        url = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s" % (
+            "",'',msg)
+        try:
+            result = requests.get(url, param, timeout=20)
+        except Exception as e:
+            print(str(e))
+
+
+
+if __name__ == "__main__":
+    e = Exam('question.txt')
+    e.post(answer=True)
+
--- Student.py
+++ Student.py
@@ -6,12 +6,14 @@ class Student():
 
     # 类的初始化方法
     def __init__(self ,name ,score):
-        # 属性
-        self.name = name
+        # 属性 全局变量
+        self.name = name.capitalize()
         self.score = score
+        print("hell init")
+
     # 方法 函数
     def description(self):
-        print(self.name +"'s score is :"+ str(self.score))
+        print(self.name +"'s score is :" + str(self.score))
 
 
 
@@ -20,8 +22,10 @@ if __name__=="__main__":
     # s0 为 Student类的对象
     s0 = Student("tom",90)
     s1 = Student("sunny",80)
-    s3 = Student("jerry",70)
-    s_list = [s0 ,s1 ,s3]
+    s2 = Student("jerry",70)
+    
+    # array 数组[ ]
+    s_list = [s0 ,s1 ,s2]
     # 循环
     for i in s_list:
         i.description()
